a63900bbe44c04c608eea554099467e6088a407e
iamraviprakash <raviprakash.coder@gmail.com>
2019-11-03 12:41:08 +0530

    Tutorial completed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
+import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 1
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { AUTH_TOKEN } from './constants';
+import { setContext } from 'apollo-link-context'
+import { split } from 'apollo-link'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+
+// 2
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000'
+})
+
+// 4
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '' 
+    }
+  }
+})
+
+// 5
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`,
+  options: {
+    reconnect:true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    }
+  }
+})
+
+// 6
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation == 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink)
+)
+
+// 3
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+})
+
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>
+  </BrowserRouter>, 
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
